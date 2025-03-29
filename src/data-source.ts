99b31830a115bf0ea03c11bf9650b697c3b19718
Cong Wang <cwang167@my.centennialcollege.ca>
2025-03-28 21:39:36 -0400

    app init

---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "123456root",
+    database: "movie_app",
+    synchronize: false,
+    logging: false,
+    entities: [],
+    migrations: [],
+    subscribers: [],
+})
+
+export default AppDataSource
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import express, {Express, Request, Response} from 'express'
+import bodyParser from 'body-parser'
+import AppDataSource from "./data-source";
+import cors from "cors";
+import dotenv from 'dotenv'
+
+const app: Express = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cors())
+dotenv.config()
+
+
+// routes
+
+
+
+// error handler
+app.use('*', (req: Request, res: Response) => {
+    return res.status(404).send({
+        message: 'NO MATCHED ROUTER'
+    })
+})
+
+
+const startServer = async () => {
+
+    try {
+        await AppDataSource.initialize()
+    }catch(e){
+        console.log(e.message)
+    }
+
+
+
+
+    const port = Number(process.env.PORT) || 8000
+    app.listen(port, () => {
+        console.log(`SERVER IS RUNNING at ${port}!`)
+    })
+}
+
+// only run server when this file is running directly
+// in testing mode, it will NOT run startServer() function
+if (require.main === module) {
+    startServer()
+}
+
+
+export default app
